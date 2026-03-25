d046b464ddb4af6b0d2250a962cf17829e74a00f
Mutawar Prabhas <prabhasmuta99@gmail.com>
2026-03-25 18:12:38 +0530

    Update SecurityConfig.java

--- frontdesk.backend/src/main/java/com/medplus/frontdesk/backend/configuration/SecurityConfig.java
+++ frontdesk.backend/src/main/java/com/medplus/frontdesk/backend/configuration/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration config = new CorsConfiguration();
-    config.setAllowedOrigins(List.of("http://localhost:3000"));
+    config.setAllowedOrigins(List.of("http://localhost:5173"));
     config.setAllowedMethods(List.of("*"));
     config.setAllowedHeaders(List.of("*"));
     config.setAllowCredentials(true);
